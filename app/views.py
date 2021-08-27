0826c82c8f332df090135c9e52b70db3fe57927e
Milind Parvatia <milind1997@gmail.com>
2021-08-27 12:12:37 +1000

    added status code

--- app/views.py
+++ app/views.py
@@ -2,33 +2,41 @@ from django.http import response
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import IAR
 
-# Create your views here.
 
+# Create your views here.
 class v1(APIView):
     def post(self, request, format=None):
+        self.data=request.data
         
-        data=request.data
-        
-        object_iar = IAR.objects.filter(tfn=data['tfn'])
+        try:
+            object_iar = IAR.objects.filter(tfn=self.data['tfn'])
 
-        if object_iar:
-            object_iar = object_iar.order_by('taxpayer_year')[0]
+            if object_iar:
+                object_iar = object_iar.order_by('taxpayer_year')[0]
 
-            diff_gross_income = abs(object_iar.gross_income - data['gross_income'])
-            
+                diff_gross_income = abs(object_iar.gross_income - self.data['gross_income'])
+                
+                return Response({
+                    'status_code': status.HTTP_200_OK,
+                    'request_id':self.data['request_id'],
+                    'match_status':True,
+                    'income_result':diff_gross_income,
+                })
+
+            else:
+                return Response({
+                    'status_code': status.HTTP_404_NOT_FOUND,
+                    'request_id':self.data['request_id'],
+                    'match_status':False,
+                    'income_result':None,
+                })
+        except:
             return Response({
-                'request_id':data['request_id'],
-                'match_status':True,
-                'income_result':diff_gross_income,
-            })
-
-        else:
-            return Response({
-                'request_id':data['request_id'],
-                'match_status':False,
-                'income_result':None,
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "error_msg": "request is bad, please recheck it!!"
             })
 
 
